c8043d7ae78fac321eb60bd2c7c1fedc3794e511
AbelMaireg <abelrighthere@gmail.com>
2025-06-05 22:54:54 +0300

    fix: validate empty string on task create title.

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,8 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTaskDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
 
   @IsBoolean()
